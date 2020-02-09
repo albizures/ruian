7eb4ba46677301827b53724dada9c2dacf41ebfb
Jose Albizures <albizures3601@gmail.com>
2020-02-09 21:34:13 +0300

    feat(lists): create useList hook to manage any list

---
+++ src/hooks/useList.ts
@@ -0,0 +1,45 @@
+import React from 'react';
+
+interface Config<T> {
+	initialIndex?: number;
+	list: T[];
+	loop?: boolean;
+}
+
+const useList = <T>(config: Config<T>) => {
+	const { initialIndex = 0, list, loop = false } = config;
+	const [index, setIndex] = React.useState(initialIndex);
+	const current = list[index];
+	const prev = list[index - 1];
+	const next = list[index + 1];
+
+	const selectNext = () => {
+		const newIndex = index + 1;
+		if (newIndex !== list.length) {
+			setIndex(newIndex);
+		} else if (loop) {
+			setIndex(0);
+		}
+	};
+
+	const selectPrev = () => {
+		const newIndex = index - 1;
+		if (newIndex !== -1) {
+			setIndex(newIndex);
+		} else if (loop) {
+			setIndex(list.length - 1);
+		}
+	};
+
+	return {
+		index,
+		prev,
+		current,
+		next,
+		selectNext,
+		selectPrev,
+		setIndex,
+	};
+};
+
+export { useList };
