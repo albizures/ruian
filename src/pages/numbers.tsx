546f27bc4d5348d4e0a792206d27a75853ca5a3a
Jose Albizures <albizures3601@gmail.com>
2020-02-27 12:44:09 +0300

    feat(numbers): support custom min, max and with decimals options

--- src/pages/numbers.tsx
+++ src/pages/numbers.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { convert } from 'number-to-words-ru';
 import Layout from '../components/Layout';
+import { useOnChange } from '../hooks';
 
 const getNumberWords = (num: number) => {
 	const hasDecimals = String(num).split('.').length > 1;
@@ -12,39 +13,53 @@ const getNumberWords = (num: number) => {
 			integer: true,
 			fractional: true,
 		},
+		showCurrency: {
+			integer: hasDecimals,
+		},
 		currency: 'number',
 	});
 };
 
-const trueOrFalse = () => Math.random() < 0.5;
+const getRandomNumber = (min: number, max: number, withDecimals) => {
+	const num = min + Math.random() * (max - min);
 
-const getRandomNumber = (min = 0, max = 100, decimals = true) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-
-	const widthDecimals = decimals || trueOrFalse();
-	const num = Math.random() * (max - min + 1);
-
-	if (widthDecimals) {
-		return Number((num + min).toFixed(getRandomNumber(0, 4, false)));
+	if (withDecimals) {
+		return Number(num.toFixed(getRandomNumber(1, 4, false)));
 	}
 
-	return Math.floor(num) + min;
+	return Math.floor(num);
 };
-const defaultNumber = getRandomNumber();
+
+const defaultMin = 0;
+const defaultMax = 100;
+const defaultWithDecimals = false;
+const defaultNumber = getRandomNumber(
+	defaultMin,
+	defaultMax,
+	defaultWithDecimals,
+);
 
 const Numbers: React.FC = () => {
+	const [withDecimals, setWithDecimals] = React.useState(defaultWithDecimals);
+	const [max, setMax] = React.useState(defaultMax);
+	const [min, setMin] = React.useState(defaultMin);
 	const [num, setNumber] = React.useState(defaultNumber);
 	const voiceRef = React.useRef<SpeechSynthesisVoice>();
 	const onPlay = () => {
 		const numberInWords = getNumberWords(num);
+		console.log(num, numberInWords);
+
 		const speech = new SpeechSynthesisUtterance(numberInWords);
 
 		speech.voice = voiceRef.current;
 
 		speechSynthesis.speak(speech);
-		setNumber(getRandomNumber());
+		setNumber(getRandomNumber(min, max, withDecimals));
 	};
+
+	const onChangeMax = useOnChange(setMax);
+	const onChangeMin = useOnChange(setMin);
+	const onChangeWithDecimals = useOnChange(setWithDecimals);
 
 	React.useEffect(() => {
 		const getVoice = () => {
@@ -65,6 +80,23 @@ const Numbers: React.FC = () => {
 	return (
 		<Layout>
 			<button onClick={onPlay}>play</button>
+			<div>
+				<label htmlFor="decimals">decimals:</label>
+				<input
+					type="checkbox"
+					id="decimals"
+					checked={withDecimals}
+					onChange={onChangeWithDecimals}
+				/>
+			</div>
+			<div>
+				<label htmlFor="min">min:</label>
+				<input type="number" id="min" value={min} onChange={onChangeMin} />
+			</div>
+			<div>
+				<label htmlFor="max">max:</label>
+				<input type="number" id="max" value={max} onChange={onChangeMax} />
+			</div>
 		</Layout>
 	);
 };
