fa5d0c2e0878d72fd80f352d65e4c56b0d58d259
Jose Albizures <albizures3601@gmail.com>
2020-02-29 10:31:01 +0300

    fix(build): fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,13 @@ const purgecss = require('@fullhuman/postcss-purgecss');
 const plugins = ['postcss-import', 'tailwindcss', 'autoprefixer'];
 
 if (process.env.NODE_ENV === 'production') {
-	plugins.push(
-		purgecss({
+	plugins.push([
+		'@fullhuman/postcss-purgecss',
+		{
 			content: ['./components/**/*.tsx', './pages/**/*.tsx'],
 			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-		}),
-	);
+		},
+	]);
 }
 
 module.exports = {
